cd656255165aae0c56aa0be3968d921d59b8e426
Rxses <35810818+rxses-x@users.noreply.github.com>
2024-12-09 13:53:52 +0100

    feat(Events): fix filter by type

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -11,12 +11,10 @@ const PER_PAGE = 9;
 
 const EventList = () => {
   const { data, error } = useData();
-  const [type, setType] = useState();
+  const [type, setType] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
+    (!type ? data?.events : data?.events.filter((event) => event.type === type)) || []
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
